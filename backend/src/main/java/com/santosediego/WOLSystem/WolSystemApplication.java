57029cf38bd6f2da4c6834b7eb5b76cab585e53b
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2022-08-08 17:03:37 -0300

    Created project

---
+++ backend/src/main/java/com/santosediego/WOLSystem/WolSystemApplication.java
@@ -0,0 +1,13 @@
+package com.santosediego.WOLSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WolSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WolSystemApplication.class, args);
+	}
+
+}
